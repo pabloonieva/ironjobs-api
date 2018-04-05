2454586b2be53f7e33ab3e4169dbfea45cbcb0a1
Antonella Briatore <antobria@gmail.com>
2018-04-05 20:50:49 +0200

    user controller create

--- models/users.model.js
+++ models/users.model.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema   = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'The company name is required']
+    require: [true, 'Name is required']
   },
-  last name: {
+  lastName: {
     type: String,
-    require: [true, 'A company logo is required']
+    require: [true, 'Last name is required']
   },
-  email: {
+  userEmail: {
     type: String,
-    require: [true, 'A name for the offer is required']
+    lowercase: true,
+    required: [true, 'Email is required'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'User needs a password']
   },
   photo: String,
   role: {
@@ -19,3 +30,29 @@ const userSchema = new mongoose.Schema({
     default : 'ADMIN'
 }
 });
+
+userSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR)
+    .then(salt => {
+      bcrypt.hash(user.password, salt)
+        .then(hash => {
+          user.password = hash;
+          return next();
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+}
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
