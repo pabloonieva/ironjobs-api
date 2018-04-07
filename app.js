b67a184b51185880fbd477b271c2043c8562e1e4
Antonella Briatore <antobria@gmail.com>
2018-04-07 15:21:15 +0200

    cors included

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const session = require('express-session');
@@ -18,7 +19,7 @@ const sessionRoutes = require('./routes/session.routes');
 
 const app = express();
 
-
+app.use(cors(corsConfig));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
