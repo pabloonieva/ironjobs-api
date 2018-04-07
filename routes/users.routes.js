9dc66650d6801b924c8b74f20d58864d87237b29
Antonella Briatore <antobria@gmail.com>
2018-04-07 12:22:00 +0200

    User crud completed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const usersController = require('../controllers/users.controller');
 
+router.get('/',  usersController.list);
 router.post('/', usersController.create);
+router.put('/edit/:id', usersController.edit);
+router.delete('/delete/:id', usersController.delete);
 
 module.exports = router;
