44d9a32f334d5cc1dd9cde7944d4bce7746399dd
pabloonieva <pablo@ironhack.com>
2018-04-07 13:03:44 +0200

    feat: fix middleware auth and routs

--- controllers/offers.controller.js
+++ controllers/offers.controller.js
@@ -3,9 +3,28 @@ const Offer = require('../models/offers.model');
 const ApiError = require('../models/api-error.model');
 
 module.exports.list = (req, res, next) => {
-   Offer.find()
-    .then(offer => res.json(offer))
-    .catch(error => next(error));
+  if(req.user.role === 'COMPANY'){
+    Offer.find({ ownerId: req.user._id })
+     .then(offers => res.json(offers))
+     .catch(error => {
+       if (error instanceof mongoose.Error.ValidationError) {
+         next(new ApiError(error.errors));
+       } else {
+         next(new ApiError(error.message, 500));
+       }
+     });
+  }
+  if(req.user.role === 'Ironhacker'){
+    Offer.find()
+     .then(offers => res.json(offers))
+     .catch(error => {
+       if (error instanceof mongoose.Error.ValidationError) {
+         next(new ApiError(error.errors));
+       } else {
+         next(new ApiError(error.message, 500));
+       }
+     });
+  }
 };
 
 module.exports.create = (req, res, next) => {
