45448ce6e6c8c3dfcf0b13faf55ed059f620ae35
Antonella Briatore <antobria@gmail.com>
2018-04-14 13:02:33 +0200

    resolved mistake

--- middleware/secure.middleware.js
+++ middleware/secure.middleware.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const ApiError = require('../models/api-error.model');
-
+const User = require('../models/users.model'); 
 
 module.exports.checkAuthentication = (req, res, next) => {
     if(req.isAuthenticated()){
@@ -17,6 +17,7 @@ module.exports.isAdmin = (req, res, next, role) => {
     next(new ApiError('Forbidden', 403));
   }
 };
+
 module.exports.isCompany = (req, res, next, role) => {
   if(req.user.role === 'COMPANY'){
     next();
--- routes/offers.routes.js
+++ routes/offers.routes.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const offersController = require('../controllers/offers.controller');
 const secureMiddleware = require('../middleware/secure.middleware');
 
-router.get('/',secureMiddleware.checkAuthentication,  offersController.list);
-router.post('/',secureMiddleware.checkAuthentication,secureMiddleware.isCompany, offersController.create);
-router.put('/edit/:id',secureMiddleware.checkAuthentication, secureMiddleware.isCompanyOrAdmin, offersController.edit);
-router.delete('/delete/:id',secureMiddleware.checkAuthentication,secureMiddleware.isCompanyOrAdmin, offersController.delete);
+router.get('/', secureMiddleware.checkAuthentication,  offersController.list);
+router.post('/', secureMiddleware.checkAuthentication, secureMiddleware.isCompany, offersController.create);
+router.put('/edit/:id', secureMiddleware.checkAuthentication, secureMiddleware.isCompanyOrAdmin, offersController.edit);
+router.delete('/delete/:id', secureMiddleware.checkAuthentication, secureMiddleware.isCompanyOrAdmin, offersController.delete);
 
 module.exports = router;
 
 // autentificacion para meter luego   ------>     secureMiddleware.checkAuthentication
-isCompanyOrAdmin
