fdd43007228465b40e927c07b50d719a09d0ec66
pabloonieva <pablo@ironhack.com>
2018-04-17 21:46:41 +0200

    hello

--- routes/offers.routes.js
+++ routes/offers.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const offersController = require('../controllers/offers.controller');
 const secureMiddleware = require('../middleware/secure.middleware');
 
-router.get('/', secureMiddleware.checkAuthentication,  offersController.list);
-router.post('/', secureMiddleware.checkAuthentication, secureMiddleware.isCompany, offersController.create);
+router.get('/', secureMiddleware.checkAuthentication, offersController.list);
+router.post('/', secureMiddleware.checkAuthentication, offersController.create);
 router.put('/edit/:id', secureMiddleware.checkAuthentication, secureMiddleware.isCompanyOrAdmin, offersController.edit);
 router.delete('/delete/:id', secureMiddleware.checkAuthentication, secureMiddleware.isCompanyOrAdmin, offersController.delete);
 
